137e9ff32c31523641cec3bc43c66b04a754dd7c
Chemozo <chemalebo@gmail.com>
2025-01-28 15:33:11 -0600

    added views with get and post

--- backend/vehicles/views.py
+++ backend/vehicles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Vehicle
+from .serializers import VehicleSerializer
 
-# Create your views here.
+class VehicleViewSet(viewsets.ModelViewSet):
+    serializer_class = VehicleSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Vehicle.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
