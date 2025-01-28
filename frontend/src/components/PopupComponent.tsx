d1b9bd903b91709c032da71be4807e09523239b7
Chemozo <chemalebo@gmail.com>
2025-01-28 16:45:31 -0600

    added popup component

---
+++ frontend/src/components/PopupComponent.tsx
@@ -0,0 +1,15 @@
+import { Typography, Paper } from "@mui/material";
+import { Vehicle } from "../types/vehicle";
+
+interface PopupContentProps {
+  vehicle: Vehicle;
+}
+
+export const PopupContent = ({ vehicle }: PopupContentProps) => {
+  return (
+    <Paper elevation={3} style={{ padding: "10px", maxWidth: "200px" }}>
+      <Typography variant="h6">{vehicle.id}</Typography>
+      <Typography variant="body2">{vehicle.plate_number}</Typography>
+    </Paper>
+  );
+};
