22d7e10d487ebbd217c96d8a3cf913c6dc5f2e11
Chemozo <chemalebo@gmail.com>
2025-01-28 15:15:50 -0600

    added base model for vehicles

--- backend/vehicles/models.py
+++ backend/vehicles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Vehicle(models.Model):
+    id = models.AutoField(primary_key=True)
+    plate_number = models.CharField(max_length=20)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    last_updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vehicles')
+
+    def __str__(self):
+        return f"{self.plate_number} | {self.owner.username}"
