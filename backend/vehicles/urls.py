c7e458aab2ef6a036e0ce76a6e6c2d3e90eb60a7
Chemozo <chemalebo@gmail.com>
2025-01-28 20:59:14 -0600

    added user view, serializer and fixed urls

--- backend/vehicles/urls.py
+++ backend/vehicles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import VehicleViewSet
+from .views import VehicleViewSet, register_user
 
 router = DefaultRouter()
 router.register(r'vehicles', VehicleViewSet, basename='vehicle')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('register/', register_user, name='register'),
+    path('', include(router.urls)),
 ]
--- backend/vehicles/views.py
+++ backend/vehicles/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from .models import Vehicle
 from .serializers import VehicleSerializer
+
+from .serializers import UserSerializer
 
 class VehicleViewSet(viewsets.ModelViewSet):
     serializer_class = VehicleSerializer
@@ -12,3 +18,19 @@ class VehicleViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        if 'username' in serializer.errors:
+            if 'unique' in serializer.errors['username'][0]:
+                return Response({'error': 'Username already exists2.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
