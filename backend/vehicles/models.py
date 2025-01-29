edf508877709ca62d83a803a28e517cc38659cb0
Chemozo <chemalebo@gmail.com>
2025-01-28 20:57:56 -0600

    added max and min values to lat and lng

--- backend/vehicles/models.py
+++ backend/vehicles/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Vehicle(models.Model):
     id = models.AutoField(primary_key=True)
     plate_number = models.CharField(max_length=20)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(validators=[MaxValueValidator(90), MinValueValidator(-90)])
+    longitude = models.FloatField(validators=[MaxValueValidator(180), MinValueValidator(-180)])
     last_updated = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vehicles')
 
